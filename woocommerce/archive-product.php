30f2248227971f416396b7bfd57145e7426df673
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-23 11:43:13 -0700

    added woocomm

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -41,5 +41,53 @@ do_action( 'woocommerce_before_main_content' );
 	?>
 </header>
 <?php
+
 echo showmodule_shortcode(83);
+
+if ( woocommerce_product_loop() ) {
+	/**
+	 * Hook: woocommerce_before_shop_loop.
+	 *
+	 * @hooked woocommerce_output_all_notices - 10
+	 * @hooked woocommerce_result_count - 20
+	 * @hooked woocommerce_catalog_ordering - 30
+	 */
+	do_action( 'woocommerce_before_shop_loop' );
+	woocommerce_product_loop_start();
+	if ( wc_get_loop_prop( 'total' ) ) {
+		while ( have_posts() ) {
+			the_post();
+			/**
+			 * Hook: woocommerce_shop_loop.
+			 */
+			do_action( 'woocommerce_shop_loop' );
+			wc_get_template_part( 'content', 'product' );
+		}
+	}
+	woocommerce_product_loop_end();
+	/**
+	 * Hook: woocommerce_after_shop_loop.
+	 *
+	 * @hooked woocommerce_pagination - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop' );
+} else {
+	/**
+	 * Hook: woocommerce_no_products_found.
+	 *
+	 * @hooked wc_no_products_found - 10
+	 */
+	do_action( 'woocommerce_no_products_found' );
+}
+/**
+ * Hook: woocommerce_after_main_content.
+ *
+ * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+ */
+do_action( 'woocommerce_after_main_content' );
+/**
+ * Hook: woocommerce_sidebar.
+ *
+ * @hooked woocommerce_get_sidebar - 10
+ */
 get_footer( 'shop' );
