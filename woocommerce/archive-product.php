bf8fbad355b25b60e72623a369029ef8a5e652ac
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-23 11:55:05 -0700

    added woocomm

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -37,7 +37,7 @@ do_action( 'woocommerce_before_main_content' );
 	 * @hooked woocommerce_taxonomy_archive_description - 10
 	 * @hooked woocommerce_product_archive_description - 10
 	 */
-	do_action( 'woocommerce_archive_description' );
+	do_action( 'woocommerce_subcategory_thumbnails' );
 	?>
 </header>
 <?php
