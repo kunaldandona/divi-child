a0ac82ed5156e0468886d75d067633bcfbadf4d7
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-23 13:02:27 -0700

    added woocomm

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,7 +1,26 @@
 <?php 
 defined( 'ABSPATH' ) || exit;
 get_header( 'shop' );
+
+?>
+
+<div class="et_pb_section et_pb_section_1 et_pb_with_background et_pb_inner_shadow et_section_regular section_has_divider et_pb_bottom_divider">
+    <div class="et_pb_row et_pb_row_0">
+        <div class="et_pb_column et_pb_column_4_4 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough et-last-child">
+            <div class="et_pb_module et_pb_image et_pb_image_0">
+			    <span class="et_pb_image_wrap "><img src="http://100.21.124.73/wp-content/uploads/2019/10/png.png" alt="" title="" srcset="http://100.21.124.73/wp-content/uploads/2019/10/png.png 1200w, http://100.21.124.73/wp-content/uploads/2019/10/png-980x980.png 980w, http://100.21.124.73/wp-content/uploads/2019/10/png-480x480.png 480w" sizes="((min-width: 0px) and (max-width: 480px)) 480px, ((min-width: 481px) and (max-width: 980px)) 980px, (min-width: 981px) 1200px, 100vw" data-pagespeed-url-hash="4217099110" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></span>
+			</div>
+            <div class="et_pb_module et_pb_text et_pb_text_0 et_pb_bg_layout_light  et_pb_text_align_center">
+				<div class="et_pb_text_inner"><h1>Practor &amp; Gamble</h1></div>
+			</div> <!-- .et_pb_text -->
+		</div> <!-- .et_pb_column -->	
+	</div> <!-- .et_pb_row -->
+	<div class="et_pb_bottom_inside_divider"></div>
+</div>
+
+
+<?php
 woocommerce_category_image();
-echo showmodule_shortcode(83);
+// echo showmodule_shortcode(83);
 get_footer( 'shop' );
 
