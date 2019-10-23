968bcccd9d7ca2c6c490afc76c582676bc0cff13
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-23 14:11:37 -0700

    updated archive woocommerce

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -8,10 +8,12 @@ get_header( 'shop' );
     <div class="et_pb_row et_pb_row_0">
         <div class="et_pb_column et_pb_column_4_4 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough et-last-child">
             <div class="et_pb_module et_pb_image et_pb_image_0">
-			    <span class="et_pb_image_wrap "><img src="http://100.21.124.73/wp-content/uploads/2019/10/png.png" alt="" title="" srcset="http://100.21.124.73/wp-content/uploads/2019/10/png.png 1200w, http://100.21.124.73/wp-content/uploads/2019/10/png-980x980.png 980w, http://100.21.124.73/wp-content/uploads/2019/10/png-480x480.png 480w" sizes="((min-width: 0px) and (max-width: 480px)) 480px, ((min-width: 481px) and (max-width: 980px)) 980px, (min-width: 981px) 1200px, 100vw" data-pagespeed-url-hash="4217099110" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></span>
-			</div>
+			    <span class="et_pb_image_wrap ">
+                    <?php woocommerce_category_image(); ?>
+                </span>
+                </div>
             <div class="et_pb_module et_pb_text et_pb_text_0 et_pb_bg_layout_light  et_pb_text_align_center">
-				<div class="et_pb_text_inner"><h1>Practor &amp; Gamble</h1></div>
+				<div class="et_pb_text_inner"><h1><?php woocommerce_page_title(); ?></h1></div>
 			</div> <!-- .et_pb_text -->
 		</div> <!-- .et_pb_column -->	
 	</div> <!-- .et_pb_row -->
@@ -20,7 +22,52 @@ get_header( 'shop' );
 
 
 <?php
-woocommerce_category_image();
+if ( woocommerce_product_loop() ) {
+	/**
+	 * Hook: woocommerce_before_shop_loop.
+	 *
+	 * @hooked woocommerce_output_all_notices - 10
+	 * @hooked woocommerce_result_count - 20
+	 * @hooked woocommerce_catalog_ordering - 30
+	 */
+	do_action( 'woocommerce_before_shop_loop' );
+	woocommerce_product_loop_start();
+	if ( wc_get_loop_prop( 'total' ) ) {
+		while ( have_posts() ) {
+			the_post();
+			/**
+			 * Hook: woocommerce_shop_loop.
+			 */
+			do_action( 'woocommerce_shop_loop' );
+			wc_get_template_part( 'content', 'product' );
+		}
+	}
+	woocommerce_product_loop_end();
+	/**
+	 * Hook: woocommerce_after_shop_loop.
+	 *
+	 * @hooked woocommerce_pagination - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop' );
+} else {
+	/**
+	 * Hook: woocommerce_no_products_found.
+	 *
+	 * @hooked wc_no_products_found - 10
+	 */
+	do_action( 'woocommerce_no_products_found' );
+}
+/**
+ * Hook: woocommerce_after_main_content.
+ *
+ * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+ */
+do_action( 'woocommerce_after_main_content' );
+/**
+ * Hook: woocommerce_sidebar.
+ *
+ * @hooked woocommerce_get_sidebar - 10
+ */
 echo showmodule_shortcode(83);
 get_footer( 'shop' );
 
