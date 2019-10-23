cfbf4158559b2ed297396a09407067f76009628d
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-23 11:41:48 -0700

    added woocomm

--- functions.php
+++ functions.php
@@ -3,3 +3,9 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function showmodule_shortcode($moduleid) {
+    extract(shortcode_atts(array('id' =>'*'),$moduleid)); 
+    return do_shortcode('[et_pb_section global_module="'.$id.'"][/et_pb_section]');
+}
+add_shortcode('showmodule', 'showmodule_shortcode');
