22dd64949088b3d28e28a117c2a2db7a1a3b15ef
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-24 16:42:13 -0700

    Fixed animation trans

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
 
     $('#flexxi-logo').click((e) => {
         e.preventDefault();
-        $('#main-flexxi-submenu').toggleClass("display-none");
+        $('#main-flexxi-submenu').toggleClass("display-none", 1000);
     })
 
 });
