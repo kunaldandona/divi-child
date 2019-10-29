787b8c76920c02c40e2cf59a3da13f1fd8b0a808
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-29 11:52:55 -0700

    added front

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ jQuery(document).ready(function ($) {
     $('#flexxi-logo').click((e) => {
         e.preventDefault();
         $('#main-flexxi-submenu').toggleClass("display-none", 1000);
-    })
+    });
+
+    let colors = ["#A773FF", "#7568E8", "#689FE8", "#73D4FF"];
+
+    colorChange = () => {
+        colors.push(colors.shift());
+        for (let i = 0; i < colors.length; i++) {
+            $(".container div:nth-child(" + (i + 1) + ")").css(
+                "background-color",
+                colors[i]
+            );
+        }
+    };
+
+    setInterval(() => {
+        colorChange();
+    }, 1000);
 
 });
