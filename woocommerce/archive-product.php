84bbb71d3cad719d56aaa85dc60065230ce96384
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-29 10:46:59 -0700

    added description

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -67,7 +67,7 @@ do_action( 'woocommerce_after_main_content' );
 ?>
 <div class="row-archive">
 	<div class="cat-description">
-		<?php woocommerce_page_title(); ?>
+		<h1><?php woocommerce_page_title(); ?></h1>
 		<?php woocommerce_taxonomy_archive_description(); ?>
 	</div>
 </div>
