7f6403e084b5b9308ee78ab8dd366c643e0c2aa1
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-29 16:45:00 -0700

    added nav scroll effect

--- js/main.js
+++ js/main.js
@@ -21,4 +21,44 @@ jQuery(document).ready(function ($) {
         colorChange();
     }, 1000);
 
+    // Hiding Header on scroll Down
+
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $('header').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('header').removeClass('nav-up');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
 });
