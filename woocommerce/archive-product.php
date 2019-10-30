95386ebda9360c643f940a77d293eae1d37d6a10
Kunal Dandona <kunaldandona009@gmail.com>
2019-10-30 11:57:49 -0700

    fixing conditional description

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -67,12 +67,14 @@ do_action( 'woocommerce_after_main_content' );
  */
 ?>
 <?php
-echo '<div class="row-archive"><div class="cat-description"><h1>';
-echo woocommerce_page_title();
-echo '</h1>';
-echo woocommerce_taxonomy_archive_description(); 
-echo flexxi_woocommerce_taxonomy_archive_description();
-echo '</div></div>';
+	if(woocommerce_taxonomy_archive_description()){
+		echo '<div class="row-archive"><div class="cat-description"><h1>';
+		echo woocommerce_page_title();
+		echo '</h1>';
+		echo woocommerce_taxonomy_archive_description(); 
+		echo flexxi_woocommerce_taxonomy_archive_description();
+		echo '</div></div>';
+	}
 ?>
 <?php
 
