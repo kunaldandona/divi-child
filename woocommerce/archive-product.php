8f2e344893857b4c0fff8680361c4d36e4138c6a
Kunal Dandona <kunaldandona009@gmail.com>
2019-11-05 15:04:55 -0800

    offer page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,29 +1,24 @@
 <?php
-/**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
- */
 defined( 'ABSPATH' ) || exit;
 get_header( 'shop' );
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
 do_action( 'woocommerce_before_main_content' );
+$term = get_queried_object();
+$termID = $term->term_id;
+$post_id = 'product_cat_'.$termID;
+
+// Custom Field Text
+$text = get_field("text_category", $post_id);
+
+$categorybanner = get_field('background-hero', $post_id);
+if($categorybanner){
+	$heroBanner = '<div class="hero-banner"><img src="'. $categorybanner .'" /></div>';
+}
+
+$children = get_terms( $term->taxonomy, array(
+	'parent'    => $term->term_id,
+	'hide_empty' => false
+	));
+	
 ?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
