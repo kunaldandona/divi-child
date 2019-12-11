0c48dd6fe0606f8274fb7a5ac1fbe5c62ad5fd33
Kunal Dandona <kunaldandona009@gmail.com>
2019-12-11 14:07:16 -0800

    js

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -19,6 +19,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 global $product;
 ?>
+<div class="no-flex">
 <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) );?>"><?php echo $product->get_price_html(); ?></p>
 <?php
 echo do_shortcode('[et_pb_row global_module="6038"][/et_pb_row]'); ?>
+</div>
